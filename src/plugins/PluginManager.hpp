9255d75934d66e902a9f80112222414eab283a95
KinetixFitt <m.ezequiel.gonzalez25@gmail.com>
2026-09-17 23:11:14 +0900

    security(plugins): extend plugin descriptor trust metadata

--- src/plugins/PluginManager.hpp
+++ src/plugins/PluginManager.hpp
@@ -1,18 +1,24 @@
 #pragma once
+
 #include <QJsonObject>
 #include <QString>
 #include <QStringList>
 #include <QVector>
 
 namespace ccos::plugins {
+
 struct PluginDescriptor {
     QString id;
     QString name;
     QString version;
     QString apiVersion;
     QString library;
+    QString resolvedLibraryPath;
+    QString sha256;
     QStringList capabilities;
     QJsonObject raw;
+    bool libraryExists = false;
+    bool hashVerified = false;
 };
 
 class PluginManager {
@@ -20,4 +26,5 @@ public:
     static QVector<PluginDescriptor> scan(const QStringList& directories, QStringList* diagnostics = nullptr);
     static bool isCompatible(const PluginDescriptor& descriptor, const QString& apiVersion = QStringLiteral("1"));
 };
-}
+
+} // namespace ccos::plugins
