2b4087ae612b4e4be874d45763493896f5fc8fa1
KinetixFitt <m.ezequiel.gonzalez25@gmail.com>
2026-09-17 10:57:55 +0900

    feat(media): add real ffmpeg proxy generation API

---
+++ src/media/ProxyManager.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include "media/MediaAsset.hpp"
+#include <QString>
+
+namespace ccos::media {
+
+enum class ProxyPreset { Quarter, Half, P720, P1080 };
+
+class ProxyManager {
+public:
+    static QString proxyPath(const MediaCache& cache, const MediaAsset& asset, ProxyPreset preset);
+    static bool createProxy(const MediaAsset& asset, const QString& outputPath,
+                            ProxyPreset preset = ProxyPreset::Quarter,
+                            const QString& executable = QStringLiteral("ffmpeg"),
+                            QString* error = nullptr);
+    static bool ensureProxy(const MediaCache& cache, const MediaAsset& asset,
+                            ProxyPreset preset = ProxyPreset::Quarter,
+                            const QString& executable = QStringLiteral("ffmpeg"),
+                            QString* error = nullptr);
+};
+}
