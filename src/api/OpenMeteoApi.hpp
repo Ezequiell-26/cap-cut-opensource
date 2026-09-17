e4cb919c289136b102208926800fd50e17604a21
KinetixFitt <m.ezequiel.gonzalez25@gmail.com>
2026-09-18 07:35:30 +0900

    feat(api): add bounded Open-Meteo geocoding support

--- src/api/OpenMeteoApi.hpp
+++ src/api/OpenMeteoApi.hpp
@@ -1,8 +1,21 @@
 #pragma once
 #include <QString>
+
+class QJsonDocument;
 #include <QVariantMap>
+#include <QVector>
 
 namespace ccos::api {
+
+struct GeocodedLocation {
+    QString name;
+    QString country;
+    QString countryCode;
+    QString admin1;
+    QString timezone;
+    double latitude = 0.0;
+    double longitude = 0.0;
+};
 
 struct WeatherData {
     QString location;
@@ -16,6 +29,8 @@ struct WeatherData {
 class OpenMeteoApi {
 public:
     static WeatherData getCurrentWeather(double latitude, double longitude);
+    static QVector<GeocodedLocation> searchLocations(const QString& name, int count = 10);
+    static QVector<GeocodedLocation> parseGeocoding(const QJsonDocument& document);
     static QVector<QVariantMap> getForecast(double latitude, double longitude, int days = 7);
 };
 
