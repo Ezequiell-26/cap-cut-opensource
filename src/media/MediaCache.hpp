e925b242fdfa1e8d1e2edb01b0019e46a4257a93
KinetixFitt <m.ezequiel.gonzalez25@gmail.com>
2026-09-17 23:28:16 +0900

    security: harden media cache API

--- src/media/MediaCache.hpp
+++ src/media/MediaCache.hpp
@@ -1,21 +1,31 @@
 #pragma once
+
 #include <QByteArray>
 #include <QString>
 #include <cstddef>
 
 namespace ccos::media {
+
 class MediaCache {
 public:
     explicit MediaCache(QString root, std::size_t maxBytes = 1024ULL * 1024ULL * 1024ULL);
+
     [[nodiscard]] QString keyFor(const QString& source, const QString& variant) const;
     [[nodiscard]] QString pathFor(const QString& source, const QString& variant, const QString& extension) const;
+
+    // Writes are crash-safe: the destination is replaced atomically on commit.
     bool write(const QString& source, const QString& variant, const QByteArray& data, const QString& extension);
     [[nodiscard]] QByteArray read(const QString& source, const QString& variant, const QString& extension) const;
     bool remove(const QString& source, const QString& variant, const QString& extension);
     void clear();
+
 private:
+    [[nodiscard]] static QString normalizeExtension(const QString& extension);
+    [[nodiscard]] static bool isCacheFileName(const QString& fileName);
     void enforceLimit();
+
     QString root_;
     std::size_t maxBytes_;
 };
-}
+
+} // namespace ccos::media
