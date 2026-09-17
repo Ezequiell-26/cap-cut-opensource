adc27446f7c0ecc6aaa0f91828de6f563609c7bc
KinetixFitt <m.ezequiel.gonzalez25@gmail.com>
2026-09-18 00:17:30 +0900

    feat: add slip and ripple-delete timeline operations

--- src/timeline/TimelineEditor.hpp
+++ src/timeline/TimelineEditor.hpp
@@ -7,6 +7,8 @@ public:
     static bool trimClip(Track& track, std::size_t clipIndex, ccos::core::Time newIn, ccos::core::Time newOut);
     static bool splitClip(Track& track, std::size_t clipIndex, ccos::core::Time timelineOffset);
     static bool moveClip(Track& track, std::size_t clipIndex, ccos::core::Time newStart);
+    static bool slipClip(Track& track, std::size_t clipIndex, ccos::core::Time sourceDelta);
     static bool deleteClip(Track& track, std::size_t clipIndex);
+    static bool rippleDelete(Track& track, std::size_t clipIndex);
 };
 }
