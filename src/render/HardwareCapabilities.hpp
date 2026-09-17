f128d9215c06302cd80579563551cdceacbaed86
KinetixFitt <m.ezequiel.gonzalez25@gmail.com>
2026-09-18 00:01:43 +0900

    feat: expand hardware encoder capability model

--- src/render/HardwareCapabilities.hpp
+++ src/render/HardwareCapabilities.hpp
@@ -1,17 +1,26 @@
 #pragma once
+#include <QString>
 #include <QStringList>
 
 namespace ccos::render {
+
 struct HardwareCapabilities {
     QStringList encoders;
     bool hasNvidia = false;
     bool hasIntel = false;
     bool hasAmd = false;
+    bool hasVideoToolbox = false;
+    bool hasVaapi = false;
+
     [[nodiscard]] bool supports(const QString& encoder) const { return encoders.contains(encoder); }
+
+    [[nodiscard]] QString preferredH264Encoder(bool hardwareAcceleration = true) const;
+    [[nodiscard]] QString preferredHevcEncoder(bool hardwareAcceleration = true) const;
 };
 
 class HardwareCapabilitiesProbe {
 public:
     static HardwareCapabilities detect(const QString& ffmpegExecutable = QStringLiteral("ffmpeg"));
 };
-}
+
+} // namespace ccos::render
