68b1f556e7a5186c56b50f365f3a8e743a8be8e0
KinetixFitt <m.ezequiel.gonzalez25@gmail.com>
2026-09-17 10:48:16 +0900

    feat(media): implement stable-id asset constructor

--- src/media/MediaAsset.cpp
+++ src/media/MediaAsset.cpp
@@ -4,5 +4,6 @@
 namespace ccos::media {
 MediaAsset::MediaAsset() = default;
 MediaAsset::MediaAsset(QString path) : path_(std::move(path)) { refreshName(); }
+MediaAsset::MediaAsset(ccos::core::Uuid id, QString path) : id_(std::move(id)), path_(std::move(path)) { refreshName(); }
 void MediaAsset::refreshName() { name_ = QFileInfo(path_).fileName(); }
 }
