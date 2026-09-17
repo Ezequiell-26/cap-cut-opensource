2654e769ee4358f733c7a8ca8e985bc8df3b1324
KinetixFitt <m.ezequiel.gonzalez25@gmail.com>
2026-09-17 11:04:09 +0900

    feat(project): add project-owned text layers

--- src/project/Project.hpp
+++ src/project/Project.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "media/MediaAsset.hpp"
 #include "timeline/Timeline.hpp"
+#include "text/TextLayer.hpp"
 #include "core/Uuid.hpp"
 #include <QString>
 #include <vector>
@@ -17,11 +18,15 @@ public:
     std::vector<ccos::media::MediaAsset>& assets() noexcept { return assets_; }
     [[nodiscard]] ccos::timeline::Timeline& timeline() noexcept { return timeline_; }
     [[nodiscard]] const ccos::timeline::Timeline& timeline() const noexcept { return timeline_; }
+    [[nodiscard]] const std::vector<ccos::text::TextLayer>& textLayers() const noexcept { return textLayers_; }
+    std::vector<ccos::text::TextLayer>& textLayers() noexcept { return textLayers_; }
     void addAsset(ccos::media::MediaAsset asset) { assets_.push_back(std::move(asset)); }
+    void addTextLayer(ccos::text::TextLayer layer) { textLayers_.push_back(std::move(layer)); }
 private:
     ccos::core::Uuid id_;
     QString name_;
     std::vector<ccos::media::MediaAsset> assets_;
     ccos::timeline::Timeline timeline_;
+    std::vector<ccos::text::TextLayer> textLayers_;
 };
 }
