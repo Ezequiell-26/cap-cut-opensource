fc4b01e33acb588fbaaf95a8277380c55067cfb7
KinetixFitt <m.ezequiel.gonzalez25@gmail.com>
2026-09-17 23:36:13 +0900

    feat: add bounded batch media import API

--- src/media/MediaImporter.hpp
+++ src/media/MediaImporter.hpp
@@ -1,11 +1,14 @@
 #pragma once
 #include "media/MediaAsset.hpp"
+#include <QString>
 #include <QStringList>
+#include <cstddef>
 #include <vector>
 
 namespace ccos::media {
 class MediaImporter {
 public:
     static std::vector<MediaAsset> importFiles(const QStringList& paths);
+    static std::vector<MediaAsset> importFiles(const QStringList& paths, std::size_t maxFiles, QString* error);
 };
 }
