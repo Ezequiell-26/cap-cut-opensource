f537bfbba0395f72591f76c1f192506dfba97ac0
KinetixFitt <m.ezequiel.gonzalez25@gmail.com>
2026-09-17 10:48:26 +0900

    feat(timeline): implement named track constructor

--- src/timeline/Track.cpp
+++ src/timeline/Track.cpp
@@ -1,4 +1,6 @@
 #include "timeline/Track.hpp"
+
 namespace ccos::timeline {
-Track::Track(TrackType type) : type_(type), name_(type == TrackType::Video ? QStringLiteral("Video 1") : QStringLiteral("Audio 1")) {}
+Track::Track(TrackType type) : Track(type, type == TrackType::Video ? QStringLiteral("Video 1") : QStringLiteral("Audio 1")) {}
+Track::Track(TrackType type, QString name) : type_(type), name_(std::move(name)) {}
 }
