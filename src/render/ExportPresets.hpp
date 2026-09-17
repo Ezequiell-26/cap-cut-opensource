1d42e6eb5e4b05537a67063cb026980f06878afd
KinetixFitt <m.ezequiel.gonzalez25@gmail.com>
2026-09-18 00:09:58 +0900

    feat: add typed export preset catalog

---
+++ src/render/ExportPresets.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "render/ExportSettings.hpp"
+
+#include <QString>
+#include <QStringList>
+
+namespace ccos::render {
+
+enum class ExportPresetId {
+    H264_1080p30,
+    H264_1080p60,
+    H264_4K30,
+    H264_Vertical1080p30,
+    WebM_1080p30
+};
+
+struct ExportPreset {
+    ExportPresetId id;
+    QString name;
+    QString description;
+    ExportSettings settings;
+};
+
+class ExportPresetCatalog {
+public:
+    [[nodiscard]] static QList<ExportPreset> all();
+    [[nodiscard]] static ExportSettings settingsFor(ExportPresetId id);
+    [[nodiscard]] static QString nameFor(ExportPresetId id);
+    [[nodiscard]] static QString idString(ExportPresetId id);
+    [[nodiscard]] static bool fromIdString(const QString& id, ExportPresetId* result);
+};
+
+} // namespace ccos::render
