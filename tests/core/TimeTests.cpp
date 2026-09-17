371d49f9d1b3cf7f701b85a95708951a8c59b92d
KinetixFitt <m.ezequiel.gonzalez25@gmail.com>
2026-09-17 10:43:23 +0900

    test(core): cover rational timeline time

---
+++ tests/core/TimeTests.cpp
@@ -0,0 +1,20 @@
+#include "core/Time.hpp"
+#include <gtest/gtest.h>
+
+TEST(TimeTests, NormalizesFractions) {
+    const ccos::core::Time time(120, 60);
+    EXPECT_EQ(time.numerator(), 2);
+    EXPECT_EQ(time.denominator(), 1);
+}
+
+TEST(TimeTests, FrameConversionIsExact) {
+    const auto time = ccos::core::Time::fromFrames(30, 30);
+    EXPECT_EQ(time.numerator(), 1);
+    EXPECT_EQ(time.denominator(), 1);
+}
+
+TEST(TimeTests, AdditionWorksAcrossDenominators) {
+    const auto result = ccos::core::Time(1, 2) + ccos::core::Time(1, 3);
+    EXPECT_EQ(result.numerator(), 5);
+    EXPECT_EQ(result.denominator(), 6);
+}
