08b1e175357397f06dfc3b4681cc501851ddaa48
KinetixFitt <m.ezequiel.gonzalez25@gmail.com>
2026-09-17 10:57:51 +0900

    feat(project): persist clip transform speed and effects

--- src/project/ProjectSerializer.cpp
+++ src/project/ProjectSerializer.cpp
@@ -8,20 +8,35 @@
 namespace ccos::project {
 namespace {
 QJsonObject encodeTime(const ccos::core::Time& time) {
-    QJsonObject value;
-    value[QStringLiteral("n")] = static_cast<qint64>(time.numerator());
-    value[QStringLiteral("d")] = time.denominator();
-    return value;
+    return QJsonObject{{QStringLiteral("n"), static_cast<qint64>(time.numerator())},
+                       {QStringLiteral("d"), time.denominator()}};
 }
+
 ccos::core::Time decodeTime(const QJsonObject& object) {
-    return ccos::core::Time(object.value(QStringLiteral("n")).toInteger(), object.value(QStringLiteral("d")).toInt(1));
+    return ccos::core::Time(object.value(QStringLiteral("n")).toInteger(),
+                            object.value(QStringLiteral("d")).toInt(1));
+}
+
+QJsonObject encodeTransform(const ccos::timeline::TransformState& t) {
+    return QJsonObject{{QStringLiteral("x"), t.x}, {QStringLiteral("y"), t.y},
+                       {QStringLiteral("scaleX"), t.scaleX}, {QStringLiteral("scaleY"), t.scaleY},
+                       {QStringLiteral("rotation"), t.rotation}, {QStringLiteral("opacity"), t.opacity}};
+}
+
+void decodeTransform(const QJsonObject& o, ccos::timeline::TransformState& t) {
+    t.x = o.value(QStringLiteral("x")).toDouble(t.x);
+    t.y = o.value(QStringLiteral("y")).toDouble(t.y);
+    t.scaleX = o.value(QStringLiteral("scaleX")).toDouble(t.scaleX);
+    t.scaleY = o.value(QStringLiteral("scaleY")).toDouble(t.scaleY);
+    t.rotation = o.value(QStringLiteral("rotation")).toDouble(t.rotation);
+    t.opacity = o.value(QStringLiteral("opacity")).toDouble(t.opacity);
 }
 }
 
 bool ProjectSerializer::save(const Project& project, const QString& path, QString* error) {
     QJsonObject root;
     root[QStringLiteral("format")] = QStringLiteral("ccos.project");
-    root[QStringLiteral("version")] = 2;
+    root[QStringLiteral("version")] = 3;
     root[QStringLiteral("id")] = QString::fromStdString(project.id().toString());
     root[QStringLiteral("name")] = project.name();
 
@@ -56,6 +71,11 @@ bool ProjectSerializer::save(const Project& project, const QString& path, QStrin
             clipObject[QStringLiteral("start")] = encodeTime(clip.start());
             clipObject[QStringLiteral("sourceIn")] = encodeTime(clip.sourceIn());
             clipObject[QStringLiteral("sourceOut")] = encodeTime(clip.sourceOut());
+            clipObject[QStringLiteral("speed")] = clip.speed();
+            clipObject[QStringLiteral("transform")] = encodeTransform(clip.transform());
+            QJsonArray effectArray;
+            for (const auto& effect : clip.effects()) effectArray.append(effect);
+            clipObject[QStringLiteral("effects")] = effectArray;
             clips.append(clipObject);
         }
         trackObject[QStringLiteral("clips")] = clips;
@@ -68,7 +88,7 @@ bool ProjectSerializer::save(const Project& project, const QString& path, QStrin
         if (error) *error = file.errorString();
         return false;
     }
-    const auto bytes = QJsonDocument(root).toJson(QJsonDocument::Indented);
+    const QByteArray bytes = QJsonDocument(root).toJson(QJsonDocument::Indented);
     if (file.write(bytes) != bytes.size() || !file.commit()) {
         if (error) *error = file.errorString();
         return false;
@@ -94,7 +114,7 @@ bool ProjectSerializer::load(Project& project, const QString& path, QString* err
         return false;
     }
     const int version = root.value(QStringLiteral("version")).toInt(1);
-    if (version < 1 || version > 2) {
+    if (version < 1 || version > 3) {
         if (error) *error = QStringLiteral("Unsupported project version: %1").arg(version);
         return false;
     }
@@ -102,7 +122,8 @@ bool ProjectSerializer::load(Project& project, const QString& path, QString* err
     project = Project(root.value(QStringLiteral("name")).toString(QStringLiteral("Untitled Project")));
     for (const auto& value : root.value(QStringLiteral("assets")).toArray()) {
         const auto obj = value.toObject();
-        ccos::media::MediaAsset asset(ccos::core::Uuid(obj.value(QStringLiteral("id")).toString().toStdString()), obj.value(QStringLiteral("path")).toString());
+        ccos::media::MediaAsset asset(ccos::core::Uuid(obj.value(QStringLiteral("id")).toString().toStdString()),
+                                      obj.value(QStringLiteral("path")).toString());
         auto& metadata = asset.metadata();
         metadata.durationMs = obj.value(QStringLiteral("durationMs")).toInteger();
         metadata.width = obj.value(QStringLiteral("width")).toInt();
@@ -129,7 +150,13 @@ bool ProjectSerializer::load(Project& project, const QString& path, QString* err
                     ccos::core::Uuid(clipObject.value(QStringLiteral("id")).toString().toStdString()),
                     ccos::core::Uuid(clipObject.value(QStringLiteral("assetId")).toString().toStdString()));
                 clip.setStart(decodeTime(clipObject.value(QStringLiteral("start")).toObject()));
-                clip.setSourceRange(decodeTime(clipObject.value(QStringLiteral("sourceIn")).toObject()), decodeTime(clipObject.value(QStringLiteral("sourceOut")).toObject()));
+                clip.setSourceRange(decodeTime(clipObject.value(QStringLiteral("sourceIn")).toObject()),
+                                    decodeTime(clipObject.value(QStringLiteral("sourceOut")).toObject()));
+                if (version >= 3) {
+                    clip.setSpeed(clipObject.value(QStringLiteral("speed")).toDouble(1.0));
+                    decodeTransform(clipObject.value(QStringLiteral("transform")).toObject(), clip.transform());
+                    for (const auto& effect : clipObject.value(QStringLiteral("effects")).toArray()) clip.addEffect(effect.toString());
+                }
                 track.addClip(clip);
             }
             tracks.push_back(std::move(track));
