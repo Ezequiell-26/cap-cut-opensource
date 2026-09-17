74eb033c3d00537450810423e840cf19007ed2d4
KinetixFitt <m.ezequiel.gonzalez25@gmail.com>
2026-09-17 10:45:47 +0900

    test(core): cover command undo redo behavior

---
+++ tests/core/CommandStackTests.cpp
@@ -0,0 +1,25 @@
+#include "core/CommandStack.hpp"
+#include <gtest/gtest.h>
+
+namespace {
+class CounterCommand final : public ccos::core::Command {
+public:
+    explicit CounterCommand(int& value) : value_(value) {}
+    bool execute() override { ++value_; return true; }
+    void undo() override { --value_; }
+    QString name() const override { return QStringLiteral("Counter"); }
+private:
+    int& value_;
+};
+}
+
+TEST(CommandStackTests, ExecutesUndoesAndRedoes) {
+    int value = 0;
+    ccos::core::CommandStack stack;
+    EXPECT_TRUE(stack.execute(std::make_unique<CounterCommand>(value)));
+    EXPECT_EQ(value, 1);
+    EXPECT_TRUE(stack.undo());
+    EXPECT_EQ(value, 0);
+    EXPECT_TRUE(stack.redo());
+    EXPECT_EQ(value, 1);
+}
