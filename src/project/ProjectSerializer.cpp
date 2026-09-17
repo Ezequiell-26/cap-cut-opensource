7cc022715db202586af2085863c0df2d18ec3354
KinetixFitt <m.ezequiel.gonzalez25@gmail.com>
2026-09-17 10:48:50 +0900

    feat(project): persist timeline tracks clips and trim state

--- src/project/ProjectSerializer.cpp
+++ src/project/ProjectSerializer.cpp
@@ -1,17 +1,30 @@
 #include "project/ProjectSerializer.hpp"
+#include <QFile>
 #include <QJsonArray>
 #include <QJsonDocument>
 #include <QJsonObject>
 #include <QSaveFile>
 
 namespace ccos::project {
+namespace {
+QJsonObject encodeTime(const ccos::core::Time& time) {
+    QJsonObject value;
+    value[QStringLiteral("n")] = static_cast<qint64>(time.numerator());
+    value[QStringLiteral("d")] = time.denominator();
+    return value;
+}
+ccos::core::Time decodeTime(const QJsonObject& object) {
+    return ccos::core::Time(object.value(QStringLiteral("n")).toInteger(), object.value(QStringLiteral("d")).toInt(1));
+}
+}
 
 bool ProjectSerializer::save(const Project& project, const QString& path, QString* error) {
     QJsonObject root;
     root[QStringLiteral("format")] = QStringLiteral("ccos.project");
-    root[QStringLiteral("version")] = 1;
+    root[QStringLiteral("version")] = 2;
     root[QStringLiteral("id")] = QString::fromStdString(project.id().toString());
     root[QStringLiteral("name")] = project.name();
+
     QJsonArray assets;
     for (const auto& asset : project.assets()) {
         QJsonObject item;
@@ -29,6 +42,26 @@ bool ProjectSerializer::save(const Project& project, const QString& path, QStrin
         assets.append(item);
     }
     root[QStringLiteral("assets")] = assets;
+
+    QJsonArray tracks;
+    for (const auto& track : project.timeline().tracks()) {
+        QJsonObject trackObject;
+        trackObject[QStringLiteral("type")] = track.type() == ccos::timeline::TrackType::Video ? QStringLiteral("video") : QStringLiteral("audio");
+        trackObject[QStringLiteral("name")] = track.name();
+        QJsonArray clips;
+        for (const auto& clip : track.clips()) {
+            QJsonObject clipObject;
+            clipObject[QStringLiteral("id")] = QString::fromStdString(clip.id().toString());
+            clipObject[QStringLiteral("assetId")] = QString::fromStdString(clip.assetId().toString());
+            clipObject[QStringLiteral("start")] = encodeTime(clip.start());
+            clipObject[QStringLiteral("sourceIn")] = encodeTime(clip.sourceIn());
+            clipObject[QStringLiteral("sourceOut")] = encodeTime(clip.sourceOut());
+            clips.append(clipObject);
+        }
+        trackObject[QStringLiteral("clips")] = clips;
+        tracks.append(trackObject);
+    }
+    root[QStringLiteral("timeline")] = tracks;
 
     QSaveFile file(path);
     if (!file.open(QIODevice::WriteOnly)) {
@@ -60,10 +93,16 @@ bool ProjectSerializer::load(Project& project, const QString& path, QString* err
         if (error) *error = QStringLiteral("Unsupported project format");
         return false;
     }
+    const int version = root.value(QStringLiteral("version")).toInt(1);
+    if (version < 1 || version > 2) {
+        if (error) *error = QStringLiteral("Unsupported project version: %1").arg(version);
+        return false;
+    }
+
     project = Project(root.value(QStringLiteral("name")).toString(QStringLiteral("Untitled Project")));
     for (const auto& value : root.value(QStringLiteral("assets")).toArray()) {
         const auto obj = value.toObject();
-        ccos::media::MediaAsset asset(obj.value(QStringLiteral("path")).toString());
+        ccos::media::MediaAsset asset(ccos::core::Uuid(obj.value(QStringLiteral("id")).toString().toStdString()), obj.value(QStringLiteral("path")).toString());
         auto& metadata = asset.metadata();
         metadata.durationMs = obj.value(QStringLiteral("durationMs")).toInteger();
         metadata.width = obj.value(QStringLiteral("width")).toInt();
@@ -74,6 +113,31 @@ bool ProjectSerializer::load(Project& project, const QString& path, QString* err
         metadata.audioChannels = obj.value(QStringLiteral("audioChannels")).toInt();
         metadata.sampleRate = obj.value(QStringLiteral("sampleRate")).toInt();
         project.addAsset(std::move(asset));
+    }
+
+    if (version >= 2 && root.contains(QStringLiteral("timeline"))) {
+        auto& tracks = project.timeline().tracks();
+        tracks.clear();
+        for (const auto& value : root.value(QStringLiteral("timeline")).toArray()) {
+            const auto object = value.toObject();
+            const auto type = object.value(QStringLiteral("type")).toString() == QStringLiteral("audio")
+                ? ccos::timeline::TrackType::Audio : ccos::timeline::TrackType::Video;
+            ccos::timeline::Track track(type, object.value(QStringLiteral("name")).toString());
+            for (const auto& clipValue : object.value(QStringLiteral("clips")).toArray()) {
+                const auto clipObject = clipValue.toObject();
+                ccos::timeline::Clip clip(
+                    ccos::core::Uuid(clipObject.value(QStringLiteral("id")).toString().toStdString()),
+                    ccos::core::Uuid(clipObject.value(QStringLiteral("assetId")).toString().toStdString()));
+                clip.setStart(decodeTime(clipObject.value(QStringLiteral("start")).toObject()));
+                clip.setSourceRange(decodeTime(clipObject.value(QStringLiteral("sourceIn")).toObject()), decodeTime(clipObject.value(QStringLiteral("sourceOut")).toObject()));
+                track.addClip(clip);
+            }
+            tracks.push_back(std::move(track));
+        }
+        if (tracks.empty()) {
+            tracks.emplace_back(ccos::timeline::TrackType::Video);
+            tracks.emplace_back(ccos::timeline::TrackType::Audio);
+        }
     }
     return true;
 }
