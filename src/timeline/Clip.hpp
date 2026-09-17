be2eb65f660a7aac64285285cc853f17ccc93a60
KinetixFitt <m.ezequiel.gonzalez25@gmail.com>
2026-09-17 10:47:15 +0900

    feat(timeline): add source trim ranges to clips

--- src/timeline/Clip.hpp
+++ src/timeline/Clip.hpp
@@ -12,12 +12,17 @@ public:
     [[nodiscard]] const ccos::core::Uuid& assetId() const noexcept { return assetId_; }
     [[nodiscard]] ccos::core::Time start() const noexcept { return start_; }
     [[nodiscard]] ccos::core::Time duration() const noexcept { return duration_; }
+    [[nodiscard]] ccos::core::Time sourceIn() const noexcept { return sourceIn_; }
+    [[nodiscard]] ccos::core::Time sourceOut() const noexcept { return sourceOut_; }
     void setStart(ccos::core::Time value) noexcept { start_ = value; }
-    void setDuration(ccos::core::Time value) noexcept { duration_ = value; }
+    void setDuration(ccos::core::Time value) noexcept { duration_ = value; sourceOut_ = sourceIn_ + value; }
+    void setSourceRange(ccos::core::Time in, ccos::core::Time out) noexcept;
 private:
     ccos::core::Uuid id_;
     ccos::core::Uuid assetId_;
     ccos::core::Time start_;
     ccos::core::Time duration_;
+    ccos::core::Time sourceIn_;
+    ccos::core::Time sourceOut_;
 };
 }
